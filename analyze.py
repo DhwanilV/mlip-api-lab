8ebb738de7a2d2985b54ca8742097c3a2567baf2
Dhwanil <dvasani@andrew.cmu.edu>
2025-01-17 06:16:38 +0000

    first commit - added api and secret in dot env

--- analyze.py
+++ analyze.py
@@ -1,11 +1,14 @@
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from dotenv import load_dotenv
+import os
+
 import time
+load_dotenv()
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
-
+endpoint = os.getenv("ENDPOINT")
+key = os.getenv("KEY")
 credentials = CognitiveServicesCredentials(key)
 
 client = ComputerVisionClient(
